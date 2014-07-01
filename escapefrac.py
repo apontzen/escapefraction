fe14cdbb1332a68abbff9ab0eeab6ec4a2ecc278
Andrew Pontzen <a.pontzen@ucl.ac.uk>
2014-07-01 16:29:37 +0100

    Make output more comprehensible

--- escapefrac.py
+++ escapefrac.py
@@ -3,18 +3,32 @@ import numpy as np
 import healpy as hp
 import pylab as p
 import math
+import contextlib
+import sys
+import os
 
 @pynbody.derived_array
 def rhoHI(f) :
     return f['rho']*f['HI']
 
-@pynbody.derived_array
-def stellar_age(f) :
-    return f['tform'].max()-f['tform']
+
+
+class quiet(object) :
+    def __enter__(self) :
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = sys.stdout
+
+    def __exit__(self, *args) :
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+
+    
     
 def escape_fraction_from_position(f, pos, to_distance=100.0,nside=64,plot=True) :
-    f['pos']-=pos
-    im = pynbody.sph.render_spherical_image(f.gas,nside=nside,distance=to_distance,
+    with translate(f,-pos) :
+        im = pynbody.sph.render_spherical_image(f.gas,nside=nside,distance=to_distance,
                                             qty='rhoHI',kernel=pynbody.sph.Kernel2D(),
                                             out_units="m_p cm^-2",denoise=False,threaded=False)
     
@@ -22,7 +36,6 @@ def escape_fraction_from_position(f, pos, to_distance=100.0,nside=64,plot=True) 
         hp.mollview(np.log10(im))
 
     esc_frac_per_pixel = np.exp(-6.3e-18*im)
-    f['pos']+=pos
     return esc_frac_per_pixel.mean()
 
 
@@ -34,32 +47,29 @@ def dirty_escape_fraction_from_position(f, pos, to_distance=100.0,nside=64,plot=
     comes out to the correct answer."""
 
     cos_theta_cut = 1-skysize
-    print "cos_theta_cut=",cos_theta_cut
-    
-    f['pos']-=pos
-    randvec = np.random.normal(0,1,3)
-    randvec/=np.linalg.norm(randvec)
-    print "randvec=",randvec
-    cos_theta = np.dot(randvec,np.asarray(f.gas['pos']).T)/f.gas['r']
-    use_particles = cos_theta>cos_theta_cut
-    f_sub = f.gas[use_particles]
-    im = pynbody.sph.render_spherical_image(f_sub,nside=nside,distance=to_distance,
-                                            qty='rhoHI',kernel=pynbody.sph.Kernel2D(),
-                                            out_units="m_p cm^-2",denoise=False,threaded=False)
 
-    # find the pixels well within the disc
-    use_pixels = hp.query_disc(nside, randvec, math.acos(cos_theta_cut)*0.95)
-    
-    if plot :
-        hp.mollview(np.log10(im))
-        im2 = np.zeros_like(im)
-        im2[use_pixels]=im[use_pixels]
-        hp.mollview(np.log10(im2))
+    with translate(f,-pos) :
+        randvec = np.random.normal(0,1,3)
+        randvec/=np.linalg.norm(randvec)
+        cos_theta = np.dot(randvec,np.asarray(f.gas['pos']).T)/f.gas['r']
+        use_particles = cos_theta>cos_theta_cut
+        f_sub = f.gas[use_particles]
+        im = pynbody.sph.render_spherical_image(f_sub,nside=nside,distance=to_distance,
+                                                qty='rhoHI',kernel=pynbody.sph.Kernel2D(),
+                                                out_units="m_p cm^-2",denoise=False,threaded=False)
 
-   
-    im = im[use_pixels]
-    esc_frac_per_pixel = np.exp(-6.3e-18*im)
-    f['pos']+=pos
+        # find the pixels well within the disc
+        use_pixels = hp.query_disc(nside, randvec, math.acos(cos_theta_cut)*0.95)
+
+        if plot :
+            hp.mollview(np.log10(im))
+            im2 = np.zeros_like(im)
+            im2[use_pixels]=im[use_pixels]
+            hp.mollview(np.log10(im2))
+
+
+        im = im[use_pixels]
+        esc_frac_per_pixel = np.exp(-6.3e-18*im)
     return esc_frac_per_pixel.mean()
     
 def escape_fraction_from_youngstars(h,max_age='50 Myr',min_age='10 Myr',
@@ -70,14 +80,15 @@ def escape_fraction_from_youngstars(h,max_age='50 Myr',min_age='10 Myr',
     
     st = h.stars[pynbody.filt.BandPass('age',min_age,max_age)]
     print "There are",len(st),"stars"
-    if len(st)>max_num_stars :
-        st = st[np.random.permutation(np.arange(len(st)))[:max_num_stars]]
-        print "Truncated to",len(st),"stars"
+    
+    myorder = np.random.permutation(np.arange(len(st)))[:max_num_stars]
+    print "Truncated to",len(myorder),"stars"
 
-    efrac = np.empty(len(st))
-    for i in xrange(len(st)) :
-        print "Calculating for star",i
-        efrac[i]=escape_fraction_from_position(h,st['pos'][i],to_distance=1000.0,plot=False)
+    efrac = np.empty(len(myorder))
+    for i,index in enumerate(myorder) :
+        print "Calculating for star %d (%d in snapshot)"%(i,index)
+        efrac[i]=escape_fraction_from_position(h,st['pos'][index],to_distance=1000.0,plot=max_num_stars<3)
+        print " --> estimate is ",efrac[i]
 
     return efrac.mean()
 
@@ -85,16 +96,18 @@ def escape_fraction_from_youngstars(h,max_age='50 Myr',min_age='10 Myr',
 def dirty_escape_fraction_from_youngstars(h,max_age='50 Myr',min_age='10 Myr',
                                           max_num_stars=100,skysize=0.2) :
     
+    
     st = h.stars[pynbody.filt.BandPass('age',min_age,max_age)]
     print "There are",len(st),"stars"
-    if len(st)>max_num_stars :
-        st = st[np.random.permutation(np.arange(len(st)))[:max_num_stars]]
-        print "Truncated to",len(st),"stars"
+    
+    myorder = np.random.permutation(np.arange(len(st)))[:max_num_stars]
+    print "Truncated to",len(myorder),"stars"
 
-    efrac = np.empty(len(st))
-    for i in xrange(len(st)) :
-        print "Calculating for star",i
-        efrac[i]=dirty_escape_fraction_from_position(h,st['pos'][i],to_distance=1000.0,plot=False,skysize=skysize)
+    efrac = np.empty(len(myorder))
+    for i,index in enumerate(myorder) :
+        print "Calculating for star %d (number %d in snapshot)"%(i,index)
+        with quiet() :
+            efrac[i]=dirty_escape_fraction_from_position(h,st['pos'][index],to_distance=1000.0,plot=max_num_stars<3,skysize=skysize)
+        print " --> estimate is ",efrac[i]
 
     return efrac.mean()
-
